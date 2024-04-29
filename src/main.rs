5574983a9242c32886e6d75e8efcfd7a14b9a026
Martin Liska <martin.liska@hey.com>
2024-04-29 15:40:31 +0200

    Support Write header section

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use binrw::BinReaderExt;
+use binrw::{BinReaderExt, BinWrite, BinWriterExt};
 use std::{io::Cursor, net::UdpSocket};
 
-use dns_starter_rust::message::DnsHeader;
+use dns_starter_rust::message::{DnsHeader, QueryResponseIndicator};
 
 fn main() {
     let endpoint = "127.0.0.1:2053";
@@ -21,9 +21,17 @@ fn main() {
                 let dns_header = Cursor::new(&buf[..size])
                     .read_be::<DnsHeader>()
                     .expect("expected UDP package header for request");
-                println!("{dns_header:?}");
+                println!("request: {dns_header:?}");
 
-                let response = [];
+                let mut dns_response = DnsHeader::default();
+                dns_response.id = 1234;
+                dns_response.flags.set_qr(QueryResponseIndicator::Response);
+                println!("response: {dns_response:?}");
+
+                let mut output = Cursor::new(Vec::new());
+                dns_response.write_be(&mut output).unwrap();
+                let response = output.into_inner();
+
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
--- src/message.rs
+++ src/message.rs
@@ -1,72 +1,90 @@
 #![allow(dead_code)]
 
-use binrw::BinRead;
+use binrw::{BinRead, BinWrite};
 use modular_bitfield::prelude::*;
 
 #[derive(BitfieldSpecifier, Debug)]
 #[bits = 1]
-enum QueryResponseIndicator {
+pub enum QueryResponseIndicator {
     Query,
     Response,
 }
 
 #[derive(BitfieldSpecifier, Debug)]
 #[bits = 1]
-enum AuthoritativeAnswer {
+pub enum AuthoritativeAnswer {
     Other,
     Owns,
 }
 
 #[derive(BitfieldSpecifier, Debug)]
 #[bits = 1]
-enum Truncation {
+pub enum Truncation {
     No,
     LargerThan512,
 }
 
 #[derive(BitfieldSpecifier, Debug)]
 #[bits = 1]
-enum RecursionDesired {
+pub enum RecursionDesired {
     NonRecursive,
     Recursive,
 }
 
 #[derive(BitfieldSpecifier, Debug)]
 #[bits = 1]
-enum RecursionAvailable {
+pub enum RecursionAvailable {
     NotAvailable,
     Available,
 }
 
 #[bitfield]
-#[derive(BinRead, Debug)]
+#[derive(BinRead, BinWrite, Debug, Default)]
 #[br(map = Self::from_bytes)]
-pub struct DnsHeader {
-    /// Packet Identifier (ID)A random ID assigned to query packets. Response packets must reply with the same ID.
-    id: u16,
-    /// Query/Response Indicator (QR). 1 for a reply packet, 0 for a question packet.
-    qr: QueryResponseIndicator,
-    /// Operation Code (OPCODE)	Specifies the kind of query in a message.
-    opcode: B4,
-    /// Authoritative Answer (AA) 1 if the responding server "owns" the domain queried, i.e., it's authoritative.
-    authoritative: AuthoritativeAnswer,
-    /// Truncation (TC) 1 if the message is larger than 512 bytes. Always 0 in UDP responses.
-    truncation: Truncation,
+pub struct DnsHeaderFlags {
     /// Recursion Desired (RD) Sender sets this to 1 if the server should recursively resolve this query, 0 otherwise.
-    recursion_desired: RecursionDesired,
-    /// Recursion Available (RA) Server sets this to 1 to indicate that recursion is available.
-    recursion_available: RecursionAvailable,
+    pub recursion_desired: RecursionDesired,
+    /// Truncation (TC) 1 if the message is larger than 512 bytes. Always 0 in UDP responses.
+    pub truncation: Truncation,
+    /// Authoritative Answer (AA) 1 if the responding server "owns" the domain queried, i.e., it's authoritative.
+    pub authoritative: AuthoritativeAnswer,
+    /// Operation Code (OPCODE)	Specifies the kind of query in a message.
+    pub opcode: B4,
+    /// Query/Response Indicator (QR). 1 for a reply packet, 0 for a question packet.
+    pub qr: QueryResponseIndicator,
+}
+
+#[bitfield]
+#[derive(BinRead, BinWrite, Debug, Default)]
+#[br(map = Self::from_bytes)]
+pub struct DnsHeaderFlags2 {
+    /// Response Code (RCODE) Response code indicating the status of the response.
+    pub response: B4,
     /// Reserved (Z) Used by DNSSEC queries. At inception, it was reserved for future use.
     #[skip]
     __: B3,
-    /// Response Code (RCODE) Response code indicating the status of the response.
-    response: B4,
+    /// Recursion Available (RA) Server sets this to 1 to indicate that recursion is available.
+    pub recursion_available: RecursionAvailable,
+}
+
+#[derive(BinRead, BinWrite, Debug, Default)]
+pub struct DnsHeader {
+    /// Packet Identifier (ID)A random ID assigned to query packets. Response packets must reply with the same ID.
+    pub id: u16,
+
+    pub flags: DnsHeaderFlags,
+
+    pub flags2: DnsHeaderFlags2,
+
     /// Question Count (QDCOUNT) Number of questions in the Question section.
-    question_count: u16,
+    pub question_count: u16,
+
     /// Answer Record Count (ANCOUNT) Number of records in the Answer section.
-    answer_count: u16,
+    pub answer_count: u16,
+
     /// Authority Record Count (NSCOUNT)	16 bits	Number of records in the Authority section.
-    nscount: u16,
+    pub nscount: u16,
+
     /// Additional Record Count (ARCOUNT)	16 bits	Number of records in the Additional section.
-    arcount: u16,
+    pub arcount: u16,
 }
